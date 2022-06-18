3aa01bdae619f6dfe624b31ed55f0b5e493bbe61
Cengiz YILMAZ <terabilgi@gmail.com>
2022-06-18 19:41:25 +0300

    css eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { increment, decrement, incrementSenBelirle } from './counterSlice'
+import './App.css'
 
 export default function App() {
   const counter = useSelector((state) => state.counter.value) // `counter state` içindeki value değerini alıyoruz.
@@ -26,15 +27,17 @@ export default function App() {
   }
 
   return (
-    <>
-      <h1>Redux Toolkit</h1>
-      <p style={{ marginLeft: '50px' }}>{counter}</p>
-      <p>Girilen Sayı kadar arttır:</p>
-      <input type='number' onChange={onChangeHandler} value={sayi} />
-      <br/>
-      <button onClick={handleArttir}>+ Arttır</button>
-      <button onClick={handleAzalt}>- Azalt</button>
-      <button onClick={handleArttirSenBelirle}>+ {sayi} Arttır</button>
-    </>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Redux Toolkit</h1>
+        <h2>{counter}</h2>
+        <p>Girilen kadar arttır</p>
+        <input type='number' onChange={onChangeHandler} value={sayi} />
+        <br />
+        <button onClick={handleArttir}>+ Arttır</button>
+        <button onClick={handleAzalt}>- Azalt</button>
+        <button onClick={handleArttirSenBelirle}>+ {sayi} Arttır</button>
+      </header>
+    </div>
   )
 }
